342bad856a5467a47d154dc63d6e2e9ca5d8d1d9
Patrick Bergman <patrickbergman@me.com>
2017-06-20 10:41:53 +0200

    Add get teams method to match

--- classes/Match.py
+++ classes/Match.py
@@ -1,24 +1,27 @@
 class Match:
 
     def __init__(self):
-        self.teams = []
-        self.players = []
+        self.__teams = []
+        self.__players = []
 
     def addTeam(self, team):
         """Add a team to the match"""
-        self.teams.append(team)
+        self.__teams.append(team)
 
     def addPlayer(self, player):
         """Add a player to the match and the team"""
-        self.players.append(player)
+        self.__players.append(player)
         self.__addPlayerToTeam(player, player.getTeamId())
 
     def findTeamById(self, teamId, default=None):
         """return a team from the array"""
-        for team in self.teams:
+        for team in self.__teams:
             if team.getId() == teamId:
                 return team
         return default
+
+    def getTeams(self):
+        return self.__teams
 
     def __addPlayerToTeam(self, player, teamId):
         """Add a player to his/her team"""
